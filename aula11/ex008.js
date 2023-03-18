4f03cbf992b38a790290ea740999b23221001333
Marcos Furlan <123518847+furlan-marcos@users.noreply.github.com>
2023-03-18 13:41:45 -0300

    ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 80.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`);
+if(vel>60){     //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Você foi multado.`)
+}
