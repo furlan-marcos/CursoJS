8a2e14ce77614fabe53125d232b998e5bc3c3f3a
Marcos Furlan <123518847+furlan-marcos@users.noreply.github.com>
2023-03-21 21:10:43 -0300

    Exercicios da Aula 11

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -3,5 +3,5 @@ console.log(`Vivendo em ${pais}`)
 if(pais == 'Brasil'){
     console.log('Você é brasileira')
 } else{
-    console.log('Você é strangeira')
+    console.log('Você é estrangeira')
 }
