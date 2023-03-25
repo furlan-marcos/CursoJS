413218731ef4f115b1d99d22563a9e696847a2ef
Marcos Furlan <123518847+furlan-marcos@users.noreply.github.com>
2023-03-25 13:07:12 -0300

    Aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+
+do {
+    console.log(`Paaso ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+while (c <= 6) {
+    console.log(`Paaso ${c}`)
+    c ++ //c = c + 1
+}
+*/
+
+/*
+console.log("oi")
+console.log("oi")
+console.log("oi")
+console.log("oi")
+console.log("oi")
+console.log("oi")
+
+
+*/
---
+++ aula13/node_1f6ded5098c00.tmp.js
@@ -0,0 +1,23 @@
+var c = 1
+
+do {
+    console.log(`Paaso ${c}`)
+} while (c <= 6)
+
+/*
+while (c <= 6) {
+    console.log(`Paaso ${c}`)
+    c ++ //c = c + 1
+}
+*/
+
+/*
+console.log("oi")
+console.log("oi")
+console.log("oi")
+console.log("oi")
+console.log("oi")
+console.log("oi")
+
+
+*/
